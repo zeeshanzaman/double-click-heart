3ebb5f3abe96d13e8a82df4ac65c989eba6bc610
zeeshanzaman <88842812+zeeshanzaman@users.noreply.github.com>
2022-09-27 17:35:57 +0500

    create variable with the name of createHeart

--- custom.js
+++ custom.js
@@ -15,3 +15,22 @@ loveMe.addEventListener('click', (e) => {
         }
     }
 })
+
+const createHeart = (e) => {
+    const heart = document.createElement('i')
+    heart.classList.add('fas')
+    heart.classList.add('fa-heart')
+
+    const x = e.clientX
+    const y = e.clientY
+
+    const leftOffset = e.target.offsetLeft
+    const topOffset = e.target.topoffsetTop
+
+    const xInside = x - leftOffset
+    const yInside = y - topOffset
+
+    heart.style.top = `${yInside}px`
+    heart.style.left = `${xInside}px`
+
+}
